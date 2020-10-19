365f0f9d2fa5193d36969f5c7ffb83a3414b7fe7
Brandon Waddill <bwadizle@gmail.com>
2020-10-18 23:40:14 -0400

    domo B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -11,7 +11,8 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
-  res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 };
 
 const login = (request, response) => {
@@ -30,6 +31,8 @@ const login = (request, response) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong name or password' });
     }
+
+    req.session.account = Account.AccountModel.toAPI(account);
 
     return res.json({ redirect: '/maker' });
   });
@@ -63,7 +66,10 @@ const signup = (request, response) => {
 
     const savePromise = newAccount.save();
 
-    savePromise.then(() => res.json({ redirect: '/maker' }));
+    savePromise.then(() => {
+        req.session.account = Account.AccountModel.toAPI(newAccount);
+        return res.json({ redirect: '/maker' })
+    });
 
     savePromise.catch((err) => {
       console.log(err);
