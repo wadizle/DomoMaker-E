f23cea5043facaf653a90f8ea224aaf498c418b4
Brandon Waddill <bwadizle@gmail.com>
2020-10-25 23:24:58 -0400

    found the typo

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -13,9 +13,9 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
-  //if (req.headers['x-forwared-proto'] !== 'https') {
-  //  return res.redirect(`https://${req.hostname}${req.url}`);
-  //}
+  if (req.headers['x-forward-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
+  }
   return next();
 };
 
